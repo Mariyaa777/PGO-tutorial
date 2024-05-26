8555097ae6ca1bb885fb331bd362590406ca547f
Mariyaa777 <162111343+Mariyaa777@users.noreply.github.com>
2024-05-27 00:17:35 +0200

    Add files via upload

---
+++ pgo9 task 2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat fillip = new Cat("Fillip");
+      fillip.start();
+        System.out.println(Fillip.getType());
+        fillip.stop();
+    }
+}
